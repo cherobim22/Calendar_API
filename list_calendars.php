e83147a08f13ed8c2c7968fe867592fb8b1a632e
Lucas Cherobim <lucascherobim@hotmail.com>
2020-07-30 20:28:38 -0300

    update files

---
+++ list_calendars.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+header('Content-Type: application/json');
+error_reporting(E_ALL);
+
+require_once "GDGoogleClient.php";
+
+$gd_client = new GDGoogleCLient;
+
+$gd_client->setToken($_POST);
+
+$events = $gd_client->getCalendars();
+
+echo(json_encode($events));
